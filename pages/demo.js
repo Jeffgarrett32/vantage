756fbd88ceb21e4cdecb5bb608e17dbebef9dc41
Jeff Garrett <jeff.garrett32@gmail.com>
2022-04-01 08:44:54 -0400

    demo fix

--- pages/demo.js
+++ pages/demo.js
@@ -1,11 +1,4 @@
-import React, { useRef } from 'react'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import Navlesson from '../components/Navlesson'
-import GraphToggle from '../components/GraphToggle'
-import Carousel from '../components/Carousel'
-import FlipCard from '../components/Flipcard'
-import DemoIntro from '../components/DemoIntro'
-import Button from '../components/Button';
+import React from 'react'
 import Demo from '../components/Demo';
 
 function demo() {
